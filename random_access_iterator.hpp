75efb5bb46bca90286dca078bf6c35a3d42f1b25
Augustin DESVALLEES <adesvall@e1r12p15.42.fr>
2022-03-16 19:11:32 +0100

    lala

---
+++ random_access_iterator.hpp
@@ -0,0 +1,103 @@
+#ifndef RANDOM_ACCESS_ITERATOR_HPP
+# define RANDOM_ACCESS_ITERATOR_HPP
+
+template <class T>
+class	random_access_iterator
+{
+public:
+	random_access_iterator() : ptr(NULL)	{}
+	random_access_iterator(T *ptr) : ptr(ptr)	{}
+	random_access_iterator(random_access_iterator &rit) : ptr(rit.ptr)	{}
+	~random_access_iterator()	{}
+
+	random_access_iterator	&operator=(random_access_iterator	&rit)	{
+		ptr = rit.ptr;
+		return *this;
+	}
+
+	random_access_iterator	&operator++()	{
+		ptr++;
+		return *this;
+	}
+
+	random_access_iterator &operator++(int)	{
+		random_access_iterator it(*this);
+		ptr++;
+		return it;
+	}
+
+	random_access_iterator	&operator--()	{
+		ptr--;
+		return *this;
+	}
+
+	random_access_iterator &operator--(int)	{
+		random_access_iterator it(*this);
+		ptr--;
+		return it;
+	}
+
+	bool	operator==(random_access_iterator &rit)	{
+		return ptr == rit;
+	}
+
+	bool	operator!=(random_access_iterator &rit)	{
+		return ptr != rit;
+	}
+
+	T	&operator*()	{
+		return *ptr;
+	}
+	const T		&operator*() const	{
+		return *ptr;
+	}
+
+	T	*operator->()	{
+		return ptr;
+	}
+	const T	*operator->() const		{
+		return ptr;
+	}
+
+	random_access_iterator	&operator+(int n)	{
+		return random_access_iterator(ptr + n);
+	}
+
+	random_access_iterator	&operator-(int n)	{
+		return random_access_iterator(ptr - n);
+	}
+
+	friend
+	random_access_iterator	&operator+(int n, random_access_operator &rit)	{
+		return random_access_iterator(ptr + n);
+	}
+
+	int		operator-(random_access_iterator &rit)	{
+		return ptr - rit.ptr;
+	}
+
+	bool	operator<(random_access_iterator &rit)	{
+		return ptr < rit.ptr;	}
+	bool	operator<=(random_access_iterator &rit)	{
+		return ptr <= rit.ptr;	}
+	bool	operator>(random_access_iterator &rit)	{
+		return ptr > rit.ptr;	}
+	bool	operator>=(random_access_iterator &rit)	{
+		return ptr >= rit.ptr;	}
+
+	random_access_iterator	&operator+=(int n)	{
+		ptr += n;
+	}
+	random_access_iterator	&operator-=(int n)	{
+		ptr -= n;
+	}
+
+	T	&operator[](int n)	{
+		return ptr[n];
+	}
+
+private:
+	T	*ptr;
+}
+
+#endif
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: adesvall <adesvall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 00:36:33 by adesvall          #+#    #+#             */
-/*   Updated: 2022/01/14 01:20:01 by adesvall         ###   ########.fr       */
+/*   Updated: 2022/03/16 17:56:07 by adesvall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,38 @@
 
 namespace ft {
 
-template<class T, class Alloc = std::allocator<T>>
+template<class T, class Alloc = std::allocator<T> >
 class vector
 {
+	typedef T							value_type;
+	typedef Alloc						allocator_type;
+	typedef allocator_type::reference			reference;
+	typedef allocator_type::const_reference		const_reference;
+	typedef allocator_type::pointer				pointer;
+	typedef allocator_type::const_pointer		const_pointer;
+	typedef 
+	typedef typename std::iterator<std::iterator::> forward_iterator;
+	typedef typename std::iterator<std::> forward_iterator;
 
-    typedef typename std::iterator<std::iterator::> forward_iterator;
-    typedef typename std::iterator<std::> forward_iterator;
-
-private:
-	T		*data;
-    size_t	size;
 public:
 	vector(/* args */);
 	~vector();
 
-    forward_iterator begin() {
-        return iterator<;
-    }
+	forward_iterator begin() {
+		return iterator<;
+	}
 
-    backward_iterator end() {
-        return data + size;
-    }
+	backward_iterator end() {
+		return data + size;
+	}
 
-    T* rbegin() {
-        
-    }
+	T* rbegin() {
+		
+	}
+
+private:
+	T		*data;
+	size_t	size;
 };
 
 vector::vector() : data(NULL)
